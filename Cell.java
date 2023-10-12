8e6c574f78d66855243ce0c15653871bd39b48cc
was-app <wastermanavila@gmail.com>
2023-10-12 13:34:39 -0300

    Setting up Terrain and Cell

--- Cell.java
+++ Cell.java
@@ -1,18 +1,31 @@
 import java.util.Random;
 
 public class Cell {
-    float roughness,error,heliumConcentration;
-    public void setError(float[] errorBounds){
-        Random random = new Random();
-        this.error = errorBounds[0] + random.nextFloat() * (errorBounds[1] - errorBounds[0]);
-    }
+    public float roughness,heliumConcentration;
+    public float[] readingError;
+    public int isOccupied;
+    public int isBeingUsed;
 
     public void setRoughness(float[] Bounds){
         Random random = new Random();
-        this.error = Bounds[0] + random.nextFloat() * (Bounds[1] - Bounds[0]);
+        this.roughness = Bounds[0] + random.nextFloat() * (Bounds[1] - Bounds[0]);
     }
 
-    public float timeToTravel(){
+    public void setHeliumConcentration(float[] Bounds){
+        Random random = new Random();
+        this.heliumConcentration = Bounds[0] + random.nextFloat() * (Bounds[1] - Bounds[0]);
+    }
+
+    public float getTimeToTravel(){
         return this.roughness*10;
     }
+
+    public Cell(float[] errorBounds,float[] roughnessBounds, float[] heliumBounds){
+        this.readingError = errorBounds;
+        setRoughness(roughnessBounds);
+        setHeliumConcentration(heliumBounds);
+        this.isOccupied = 0;
+        this.isBeingUsed = 0;
+    }
+
 }
--- Terrain.java
+++ Terrain.java
@@ -1,4 +1,20 @@
 public class Terrain {
-    float[] errorBounds;
+    public float[] errorBounds = {0.0f, 0.1f};
+    public float[] heliumBounds = {0.0f,1f};
+    public float[] roughnessBounds = {0.0f,1.0f};
+    public int width,length;
+
+    private final Cell[][]  grid;
+
+    public Terrain(int width, int length){
+        this.width = width;
+        this.length = length;
+        this.grid = new Cell[width][length];
+        for(int i=0;i<width;i++){
+            for(int j=0;j<length;j++){
+                grid[i][j] = new Cell(this.errorBounds,this.heliumBounds,this.roughnessBounds);
+            }
+        }
+    }
 
 }
