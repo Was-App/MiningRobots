fea749b3768993834e1841f59ae197d1fb28626d
Reisdrj <davireisjesus@gmail.com>
2023-10-12 16:03:43 -0300

    Elaborating Robot Constructor

--- Robot.java
+++ Robot.java
@@ -1,6 +1,17 @@
 public class Robot {
-    private int posX;
-    private int posY;
+    private int[] robotCoordinates = new int[2];
+    private int orientation;
+    private int numberOfBarrels;
 
+    //Constructor
+    public Robot(int[] coordinates){
+        this.robotCoordinates[0] = coordinates[0];
+        this.robotCoordinates[1] = coordinates[1];
+        this.numberOfBarrels = 0;
+        this.orientation = 0;
+    }
 
+    public int[] getRobotCoordinates() {
+        return this.robotCoordinates;
+    }
 }
