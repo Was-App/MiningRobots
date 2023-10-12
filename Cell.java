7f777f8df07d88120e052158f7e21519b0709a37
was-app <wastermanavila@gmail.com>
2023-10-12 12:11:09 -0300

    Creating empty classes

--- Cell.java
+++ Cell.java
@@ -1,2 +1,18 @@
+import java.util.Random;
+
 public class Cell {
+    float roughness,error,heliumConcentration;
+    public void setError(float[] errorBounds){
+        Random random = new Random();
+        this.error = errorBounds[0] + random.nextFloat() * (errorBounds[1] - errorBounds[0]);
+    }
+
+    public void setRoughness(float[] Bounds){
+        Random random = new Random();
+        this.error = Bounds[0] + random.nextFloat() * (Bounds[1] - Bounds[0]);
+    }
+
+    public float timeToTravel(){
+        return this.roughness*10;
+    }
 }
