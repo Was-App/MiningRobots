87f1b2d00eb0f27a24c2fecf35ab7c100e33746d
Reisdrj <davireisjesus@gmail.com>
2023-10-22 12:16:06 -0300

    Modifying read input functions

--- Input.java
+++ Input.java
@@ -2,6 +2,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
