5dccb2735b5d5a6753aa2f672d863a490276de4c
Reisdrj <davireisjesus@gmail.com>
2023-10-23 08:55:58 -0300

    Finishing Input Class

--- Input.java
+++ Input.java
@@ -7,38 +7,17 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Input {
-    public JSONObject readTerrain(String jsonPath) {
+    public Object readFile(String jsonPath) {
         JSONParser parser = new JSONParser();
-        JSONObject jsonObject = null;
+        Object terrain = null;
         try {
             FileReader reader = new FileReader(jsonPath);
-            jsonObject = (JSONObject) parser.parse(reader);
+            terrain = parser.parse(reader);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException parse) {
             parse.getErrorType();
         }
-        return jsonObject;
+        return terrain;
     }
-
-    public String readGameSettings(String path){
-        StringBuilder jsonText = new StringBuilder();
-        try {
-            BufferedReader bufferedReader =
-                    new BufferedReader(new FileReader(path));
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                jsonText.append(line).append("\n");
-            }
-
-            bufferedReader.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return jsonText.toString();
-    }
-
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import org.json.simple.JSONObject;
+
 import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args){
         Input entrada = new Input();
-        String entry = entrada.readGameSettings("Game.json");
-        System.out.println(entry);
+        JSONObject jogo = (JSONObject) entrada.readFile("Game.json");
+        JSONObject times = (JSONObject) jogo.get("Times");
+        for(int i = 1; i < 3; i++){
+            JSONObject time = (JSONObject) times.get("Time" + i);
+            System.out.println(time.get("nome"));
+        }
     }
 }
