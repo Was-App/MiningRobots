75e6f3ad3e5e80a8d4f14f3f22f414ae2670a2e1
Reisdrj <davireisjesus@gmail.com>
2023-10-25 21:44:25 -0300

    Game Constructor

--- Game.java
+++ Game.java
@@ -1,11 +1,16 @@
+import java.util.ArrayList;
+
 public class Game {
     //This will run the game
     public int secondsPassed;
     public int gameTime;
     public Terrain gameTerrain;
-    public Team[] teams;
-    public Game(int gameTime, Terrain gameTerrain, Team[] teams){
-
+    public ArrayList<Team> teams;
+    public Game(int gameTime, Terrain gameTerrain, ArrayList<Team> teams){
+        this.secondsPassed = 0;
+        this.gameTime = gameTime;
+        this.gameTerrain = gameTerrain;
+        this.teams = teams;
     }
 
     public void run(){
--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
 import org.json.simple.JSONObject;
 
+import java.util.ArrayList;
+
 import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args){
 
         Read input = new Read();
+        Initialization initializator = new Initialization("Terrain.json", "Game.json");
         JSONObject gameFile = (JSONObject) input.readFile("Game.json");
         JSONObject teamsFile = (JSONObject) gameFile.get("Teams");
-        for(int i = 1; i < 3; i++){
-            JSONObject team = (JSONObject) teamsFile.get("Team" + i);
+        JSONObject terrainFile = (JSONObject) input.readFile("Terrain.json");
+        ArrayList<Team> teams = initializator.setTeams();
+        Terrain terrain = new Terrain((int) terrainFile.get("height"), (int) terrainFile.get("length"));
+        Game game = new Game((int) gameFile.get("Game Time"), terrain, teams);
+        try{
+            game.run();
+        } catch (RuntimeException e){
+            e.printStackTrace();
         }
-        Terrain terrain = new Terrain();
-        Game game = new Game();
-        game.run();
     }
 }
